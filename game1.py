11b1b1322ca66a0c0ceb133d28649c92f666df9b
molof <michaeloloughlint@gmail.com>
2016-07-08 17:04:35 -0400

    WOOT Got projectiles & enemies dying plus a win condition that is partially working!

--- game1.py
+++ game1.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 import time
 import random
 from colors import *
@@ -9,23 +10,33 @@ class Hero_Ship(pygame.sprite.Sprite):
 		super(Hero_Ship, self).__init__()
 		if (imgfile != None):
 			self.image = pygame.image.load(imgfile)
-			self.rect = self.image.get_rect()
-	
+		self.change_x = 0
+		self.change_y = 0
+		self.rect = self.image.get_rect()
+
 	def setPos(self, x, y):
 		self.rect.x = x
 		self.rect.y = y
 
-	def moveUp(self):
-		self.rect.y -= 10
+	def update(self):
+		self.rect.x += self.change_x
+		self.rect.y += self.change_y
+	
+	def goLeft(self):
+		self.change_x -= 6
+	
+	def goRight(self):
+		self.change_x += 6
+	
+	def goUp(self):
+		self.change_y -= 6
+	
+	def goDown(self):
+		self.change_y += 6
 
-	def moveDown(self):
-		self.rect.y += 10
-
-	def moveLeft(self):
-		self.rect.x -= 10
-
-	def moveRight(self):
-		self.rect.x += 10
+	def stop(self):
+		self.change_x = 0
+		self.change_y = 0
 
 class Enemy_Ship_Basic(pygame.sprite.Sprite):
 	def __init__(self, imgfile):
@@ -37,18 +48,19 @@ class Enemy_Ship_Basic(pygame.sprite.Sprite):
 	def setPos(self, x, y):
 		self.rect.x = x
 		self.rect.y = y
+	def update(self):
+		return
 
 class Projectile_Basic(pygame.sprite.Sprite):
 	def __init__(self):
 		super(Projectile_Basic, self).__init__()
-		WIDTH = 10
-		HEIGHT = 10
+		WIDTH = 5
+		HEIGHT = 5
 		COLOR = red
 		self.image = pygame.Surface((WIDTH, HEIGHT))
 		self.image.fill(COLOR)
 		self.rect = self.image.get_rect()
-	def launch(self): 
-		#Fires the projectile 5 pixels
+	def update(self):
 		self.rect.y -= 5
 	
 #Background Stuff--------------------------------------------------------
@@ -65,7 +77,6 @@ if (__name__ == "__main__"):
 	window_size = display_height, display_width = 800, 800
 	gameDis = pygame.display.set_mode(window_size)
 	pygame.display.set_caption('Moving Square!')
-	blockSize = 10
 	#Inititalize screen (first update)
 	pygame.display.flip()
 
@@ -81,27 +92,32 @@ if (__name__ == "__main__"):
 	enemy_ship_group = pygame.sprite.Group()
 	proj_list = pygame.sprite.Group()
 	all_sprites_group = pygame.sprite.Group()
-
 	mainChar = Hero_Ship("mai.png")
-	enemyOne = Enemy_Ship_Basic("enemyRed.png")
-	enemyTwo = Enemy_Ship_Basic("enemyRed.png")
-	enemyThree = Enemy_Ship_Basic("enemyRed.png")
+	enemies = 5
+	for q in range(enemies):
+		enemy = Enemy_Ship_Basic("enemyRed.png")
+		enemy.rect.x = random.randrange(display_width)
+		enemy.rect.y = random.randrange(display_height)
+		enemy_ship_group.add(enemy)
+		all_sprites_group.add(enemy)
 	allied_ship_group.add(mainChar)
-	enemy_ship_group.add(enemyOne, enemyTwo, enemyThree)
-	all_sprites_group.add(mainChar, enemyOne, enemyTwo, enemyThree)
-
+	all_sprites_group.add(mainChar)
+	#Initialize clock for FPS
+	theClock = pygame.time.Clock()
 	#Game loop--------------------------------------------------------------
 	def gameLoop():
-		#Variables to move square up/down
 		gameEnd = False
 		gameOver = False
+		enemies_left = [enemies]
+		enemy_count = len(enemies_left)
+		#Variables to move square up/down
 		head_x = display_width / 2
 		head_y = display_height / 2
-		headx_change = 0
-		heady_change = 0
-		#Initialize clock for FPS
-		theClock = pygame.time.Clock()
+		#headx_change = 0
+		#heady_change = 0
 		FPS = 30
+		mainChar.rect.x = display_width / 2
+		mainChar.rect.y = display_height / 2
 		while not gameEnd:
 			while gameOver == True:
 				gameDis.fill(white)
@@ -117,37 +133,53 @@ if (__name__ == "__main__"):
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					gameEnd = True
+			#Player Controlled Movement & Shooting-------------------------------------------------------
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_LEFT:
-						headx_change = -blockSize
-					elif event.key == pygame.K_RIGHT:
-						headx_change = blockSize
-					elif event.key == pygame.K_UP:
-						heady_change = -blockSize
-					elif event.key == pygame.K_DOWN:
-						heady_change = blockSize
-					elif event.key == pygame.K_SPACE:
-						basicProj = Projectile_Basic()
-						basicProj.rect.x = mainChar.rect.x
-						basicProj.rect.y = mainChar.rect.y
-						all_sprites_group.add(basicProj)
-						basicProj.launch()
+						mainChar.goLeft()
+					if event.key == pygame.K_RIGHT:
+						mainChar.goRight()
+					if event.key == pygame.K_UP:
+						mainChar.goUp()
+					if event.key == pygame.K_DOWN:
+						mainChar.goDown()
+					if event.key == pygame.K_SPACE:
+						#Fire Projectile (BASIC)
+						projectile = Projectile_Basic()
+						projectile.rect.x = mainChar.rect.x
+						projectile.rect.y = mainChar.rect.y
+						all_sprites_group.add(projectile)
+						proj_list.add(projectile)
 				if event.type == pygame.KEYUP:
-					if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-						headx_change = 0
-					elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-						heady_change = 0
+					if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+						mainChar.stop()
+				if enemy_count == 0:
+					gameOver = True
+			#head_x += headx_change
+			#head_y += heady_change
+			all_sprites_group.update()
+			#Player/Enemy Interaction!----------------------------------------------
+			for projectile in proj_list:
+				hit_list = pygame.sprite.spritecollide(projectile, enemy_ship_group, True)
+				for enemy in hit_list:
+					proj_list.remove(projectile)
+					all_sprites_group.remove(projectile)
+					enemy_count -= 1
+				if projectile.rect.y < 0:
+					proj_list.remove(projectile)
+					all_sprites_group.remove(projectile)
 
-			#Boundaries & Refresh Background---------------------------------------
-			if head_x > display_width:
-				head_x = 0 
-			elif head_x < 0:
-				head_x = display_width
-			elif head_y > display_height:
-				head_y = 0
-			elif head_y < 0:
-				head_y = display_height
+			#Boundaries & Refresh Background--------------------------------------------------
+			if mainChar.rect.x > display_width:
+				mainChar.rect.x = 0 
+			elif mainChar.rect.x < 0:
+				mainChar.rect.x = display_width
+			elif mainChar.rect.y > display_height:
+				mainChar.rect.y = 0
+			elif mainChar.rect.y < 0:
+				mainChar.rect.y = display_height
 			gameDis.fill(black)
+
 			#Drawing Background-----------------------------------------------------
 			for item in starList:
 				item[1] += 1
@@ -156,22 +188,11 @@ if (__name__ == "__main__"):
 					item[1] = -3
 					item[0] = random.randrange(800)
 
-			#Movement!--------------------------------------------------------------
-			head_x += headx_change
-			head_y += heady_change
-
-			#Drawing Sprites
-			mainChar.setPos(head_x, head_y)
-			enemyOne.setPos(200, 200)
-			enemyTwo.setPos(150, display_height/3)
-			enemyThree.setPos(display_width/2, display_height/4)
+			#Setting Positions & Drawing Sprites--------------------------------------------------------
+			#mainChar.setPos(head_x, head_y)
 			all_sprites_group.draw(gameDis)
+			#Final Update
 			pygame.display.update()
-
-			#Player/Enemy Interaction!----------------------------------------------
-			#for basicProj in 
-			#ships_destroyed_list = pygame.sprite.spritecollide(basicProj, ship_group, True)
-
 			#FPS-------------------------------------------------------------------
 			theClock.tick(FPS)
 		#Quit pygame and python
